e1e10a86818e99eb2b0832d5ec6c8932cb4f7726
Daniel <dani.aizenberg47@gmail.com>
2021-03-09 16:32:54 +0200

    change imports

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Provider } from "react-redux";
 
 import { store } from "./redux";
@@ -24,7 +23,7 @@ const App = () => {
           <div>
             <h1>TODO List</h1>
             <TodoInput />
-            <br/>
+            <br />
             <TodoSearch />
             <TodoList />
             <TodoInfo />
